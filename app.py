91f7aae9841df9b3baea75b101c77e0f1c8f22b9
main <claudia.raphael73@gmail.com>
2025-12-09 18:10:44 -0300

    first commit

--- app.py
+++ app.py
@@ -4,27 +4,35 @@ from extensions import db
 import json
 
 # import blueprints
-from routes.products import products
-from routes.comments import comments
-from routes.users import users
+from routes.product import Product
+from routes.comment import Comment
+from routes.user import Users
 
-def create_app():
+def create_app(config_name='development'):
     app = Flask(__name__)
 
-    # config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///antigreenwashing.db'
+    # database config: db
+    if config_name == 'testing':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:' # SQLite em memória para testes (ISOLAMENTO!)
+        app.config['TESTING'] = True
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///antigreenwashing.db' # # SQLite em arquivo (Desenvolvimento/Produção)
+        
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)
+    db.init_app(app) # VINCULA o objeto 'db' ao App (com as configurações acima)
+
+    with app.app_context():
+        db.create_all()         # db.create_all() usa os modelos que foram importados acima (model.product, etc.)
+
+
 
     # Swagger configuration
-    
+
 
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
---
+++ model/user.py
@@ -0,0 +1,21 @@
+from extensions import db # Importa a instância global do Flask-SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from typing import Union
+
+# Agora herda de db.Model 
+class User(db.Model): 
+    __tablename__ = 'user' # Padrão é minúsculo
+
+    # Usando db.Column e db.Integer/String/DateTime
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+    
+    # Usando db.relationship
+    products = db.relationship("Product", back_populates="owner")
+    comments = db.relationship("Comment", back_populates="author")
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
