f9e206d9623220a1d9b19eb033dc270a8cbe05ad
main <claudia.raphael73@gmail.com>
2025-12-09 12:58:05 -0300

    create app.py config, create extensions.py to instance the database, run app.py to create the app and the sqlite database

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask, jsonify, request
 from flasgger import Swagger, swag_from
+from extensions import db
 import json
 
-app = Flask(__name__)
+# import blueprints
+from routes.products import products
+from routes.comments import comments
+from routes.users import users
 
-# Swagger configuration
+def create_app():
+    app = Flask(__name__)
+
+    # config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///antigreenwashing.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # Swagger configuration
+    
+
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- py_scripts/barcode_scanner.py
+++ py_scripts/barcode_scanner.py
@@ -5,3 +5,12 @@ import time
 cap = cv2.VideoCapture(0)
 cap.set(3, 640) # width
 cap.set(4, 480) # height
+
+used_codes = []
+
+camera = True
+while camera == True:
+    success, frame = cap.read()
+
+    for code in decode(frame):
+        print(code.type)
