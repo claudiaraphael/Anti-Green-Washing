f910af71326d7bad3e9b666b75ff3082cc8e8d8a
main <claudia.raphael73@gmail.com>
2025-12-13 15:51:36 -0300

    create session. problem with line 36: Base.metadata.create_all(engine): cannot access metadata. tried creating a base.py in models/ using from sqlalchemy.ext.declarative import declarative_base Base = declarative_base() but with no result. Will try different approach.

---
+++ models/__init__.py
@@ -0,0 +1,36 @@
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
+
+# import models
+from models.base import Base
+from models.user import User
+from models.comment import Comment
+from models.product import Product
+
+db_path = 'database/'
+
+if not os.path.exists(db_path):
+    """
+    verifies if the directory exists and creates one if it doesnt
+    """
+    os.makedirs(db_path)
+
+# url de acesso ao banco (essa é uma url de acesso ao sqlite local)
+db_url = 'sqlite:///%s/db.sqlite3' % db_path
+
+
+# cria a engine de conexão com o banco
+engine = create_engine(db_url, echo=False)
+
+# Instancia um criador de seção com o banco
+Session = sessionmaker(bind=engine)
+
+# cria o banco se ele não existir
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+
+# cria as tabelas do banco, caso não existam
+Base.metadata.create_all(engine)
