c24d1bf609a059cb69138bd27a3807145be41e33
main <claudia.raphael73@gmail.com>
2025-12-13 15:56:59 -0300

    Commentes all content from schemas/__init__.py to see if it works, It was a reexport of the schemas - causing an error: ModuleNotFoundError: No module named email_validator

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,5 +1,9 @@
+
+"""""
 from.user_schemas import UserInputSchema, UserResponseSchema
 from .product_schemas import ProductInputSchema, ProductResponseSchema
 from .comment_schemas import CommentInputSchema, CommentResponseSchema
 
-# reexportar todos os Schemas para facil,itar importação nas blueprints
+# reexportar todos os Schemas para facilitar importação nas blueprints
+
+"""
