ef3ef770c98cd62e29258f26a85f500c7fab840c
main <claudia.raphael73@gmail.com>
2025-12-17 21:35:47 -0300

    remove openfoodfacts blueprint and imports

--- routes/product_bp.py
+++ routes/product_bp.py
@@ -11,7 +11,6 @@ product_bp = Blueprint('product', __name__)
 
 # Create
 
-
 @product_bp.route('api/product', methods=['POST'])
 @swag_from({
     'tags': ['Product'],
@@ -51,22 +50,7 @@ def create_product():
     """
     Creates a new product in the database
 
-    Uses Kwargs to transfer data.
-
-    It's commonly used in APIs with Flask when you want flexibility in parameters without having to explicitly define them.
-
-    Creates an instance of the SQLAlchemy Product model. The model_dump(exclude_unset=True) converts the validated Pydantic object back to a dictionary, excluding fields that weren't set in the request. The ** unpacks the dictionary as named arguments.
-
-    Converts the SQLAlchemy `new_product` object to the Pydantic `ProductResponseSchema` (validating the response structure) and then transforms it into a dictionary with `model_dump()`.
-
-    Returns JSON with success message and product data, with HTTP status 201 (Created).
-
-    If any error occurs (Pydantic validation, database error, etc.), rolls back any pending changes in the SQLAlchemy session.
-
-    Returns formatted error as JSON with status 400 (Bad Request).
-
-    Flow summary: receives JSON → validates with Pydantic → creates SQLAlchemy model → saves to database → formats response with Pydantic → returns JSON.
-
+    
     """
     try:
         # 1. obtain data (pydantic)
@@ -294,3 +278,24 @@ def delete_product(product_id):
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 400
+"""
+notes
+
+Uses Kwargs to transfer data.
+
+    It's commonly used in APIs with Flask when you want flexibility in parameters without having to explicitly define them.
+
+    Creates an instance of the SQLAlchemy Product model. The model_dump(exclude_unset=True) converts the validated Pydantic object back to a dictionary, excluding fields that weren't set in the request. The ** unpacks the dictionary as named arguments.
+
+    Converts the SQLAlchemy `new_product` object to the Pydantic `ProductResponseSchema` (validating the response structure) and then transforms it into a dictionary with `model_dump()`.
+
+    Returns JSON with success message and product data, with HTTP status 201 (Created).
+
+    If any error occurs (Pydantic validation, database error, etc.), rolls back any pending changes in the SQLAlchemy session.
+
+    Returns formatted error as JSON with status 400 (Bad Request).
+
+    Flow summary: receives JSON → validates with Pydantic → creates SQLAlchemy model → saves to database → formats response with Pydantic → returns JSON.
+
+
+"""
